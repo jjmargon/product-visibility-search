523fa2c8774c2ea164e1c9eacc9a42c171720ca6
jjmar <jjmar@host.docker.internal>
2023-08-05 16:20:41 +0200

    Init project
    Commit with the project skeleton from a Spring Boot starter, adding
    support for Spring Boot Web, JPA, Flyway and H2 as test database

---
+++ src/main/java/com/inditex/product/visibility/search/ProductVisibilitySearchApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.product.visibility.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductVisibilitySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductVisibilitySearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/product/visibility/search/ProductVisibilitySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.product.visibility.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductVisibilitySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
