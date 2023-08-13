605f679a0899d6268b47bd509e3aa79fe8e5865c
jjmar <jjmar@CORSAIRONE>
2023-08-13 14:21:30 +0200

    Simple ProductFormatter implementation to pass TDD tests
    ProductFormatter could be implemented with more flexibility. Anyway,
    this simple implementation fulfills the exercise goal and sets the
    base just to future modification if it would be needed for changing
    requirements

--- src/main/java/com/inditex/product/visibility/search/adapter/format/ProductFormatter.java
+++ src/main/java/com/inditex/product/visibility/search/adapter/format/ProductFormatter.java
@@ -1,15 +1,17 @@
 package com.inditex.product.visibility.search.adapter.format;
 
 import java.util.List;
-
-import org.apache.commons.lang3.NotImplementedException;
+import java.util.stream.Collectors;
 
 import com.inditex.product.visibility.search.domain.Product;
 
 public class ProductFormatter {
+	
+	private static String COMMA_SEPARATOR_CHARACTER = ",";
 
 	public static String format(List<Product> products) {
-		throw new NotImplementedException();
+		return products.stream().map(Product::getId).map(String::valueOf).collect(
+				Collectors.joining(COMMA_SEPARATOR_CHARACTER));
 	}
 
 }
